8eea7bdbb6a0553a11367d5ea164acc025e32b95
Olloyor <muhammadnabiyevolloyor@gmail.com>
2026-07-02 16:58:53 +0500

    feat: add slash module public API

---
+++ agent/slash/index.ts
@@ -0,0 +1,4 @@
+export { SlashRegistry, loadBuiltins, type SlashCommand } from "./registry.js"
+export { matchCommands } from "./matcher.js"
+export { dispatchSlashCommand } from "./dispatch.js"
+export { SlashMenu, type MenuMode } from "./menu.js"
